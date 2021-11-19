aead60c12cf2f02501865340041fc263f0f995e7
SamirFarhat17 <samirfarhat17@gmail.com>
2021-11-19 11:36:52 -0500

    All downloads and starter script

--- tttanalyzer.cpp
+++ tttanalyzer.cpp
@@ -1,15 +1,15 @@
 // Copyright 2021 Muhammad Fahad Farid fahadf@bu.edu
 // Copyright 2021 Samir Farhat Dominguez safarhat@bu.edu
 // Copyright 2021 Kevin Vogt-Lowell kjv@bu.edu
+
 #include <iostream>
 #include <string>
-using std::string;
-using std::swap;
-using std::stoi;
-using std::endl;
-using std::cout;
+#include <array>
+#include <vector>
+#include <map>
+#include "movedef.h"
 
 int main(int argc, char* argv[]) {
-
+    return 0;
 }
 
