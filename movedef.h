6bef5f64d5e137cb8409ae9febd4e542c914c86c
SamirFarhat17 <samirfarhat17@gmail.com>
2021-11-19 11:34:30 -0500

    All downloads and starter script

---
+++ movedef.h
@@ -0,0 +1,6 @@
+
+struct Move {
+    int r;
+    int c;
+    char player;
+};
---
+++ tttanalyzer.cpp
@@ -0,0 +1,15 @@
+// Copyright 2021 Muhammad Fahad Farid fahadf@bu.edu
+// Copyright 2021 Samir Farhat Dominguez safarhat@bu.edu
+// Copyright 2021 Kevin Vogt-Lowell kjv@bu.edu
+#include <iostream>
+#include <string>
+using std::string;
+using std::swap;
+using std::stoi;
+using std::endl;
+using std::cout;
+
+int main(int argc, char* argv[]) {
+
+}
+
